363a8b59a94019f7d32eb9dce2d255e73e1221b9
madong <viaimperiale@gmail.com>
2023-04-26 00:31:05 +0200

    feat: add routes and views

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,67 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    children: [
+      {
+        path: 'search',
+        name: 'search',
+        component: () => import(/* webpackChunkName: "search" */ '../views/SearchView.vue')
+      },
+      {
+        path: 'product/:id',
+        name: 'product',
+        component: () => import(/* webpackChunkName: "product" */ '../views/ProductView.vue')
+      },
+      {
+        path: 'categories',
+        name: 'categories',
+        component: () => import(/* webpackChunkName: "categories" */ '../views/CategoriesView.vue')
+      },
+      {
+        path: 'account',
+        name: 'account',
+        component: () => import(/* webpackChunkName: "account" */ '../views/AccountView.vue'),
+        children: [
+          {
+            path: 'profile',
+            name: 'profile',
+            component: () => import(/* webpackChunkName: "profile" */ '../views/ProfileView.vue')
+          },
+          {
+            path: 'orders',
+            name: 'orders',
+            component: () => import(/* webpackChunkName: "orders" */ '../views/OrdersView.vue'),
+            children: [
+              {
+                path: 'order/:id',
+              }
+            ]
+          },
+          {
+            path: 'security',
+            name: 'security',
+            component: () => import(/* webpackChunkName: "security" */ '../views/SecurityView.vue')
+          }
+        ],
+      },
+      {
+        path: 'cart',
+        name: 'cart',
+        component: () => import(/* webpackChunkName: "cart" */ '../views/CartView.vue')
+      },
+      {
+        path: 'checkout',
+        name: 'checkout',
+        component: () => import(/* webpackChunkName: "checkout" */ '../views/CheckoutView.vue')
+      },
+      {
+        path: 'order-completed',
+        name: 'order-completed',
+        component: () => import(/* webpackChunkName: "order-complete" */ '../views/OrderCompletedView.vue')
+      }
+
+    ]
   },
 ]
 
