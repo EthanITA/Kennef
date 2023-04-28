4facbd879a75eebc30c7548751d4a1e0b56400f3
madong <viaimperiale@gmail.com>
2023-04-28 21:26:05 +0200

    feat: add NotFoundContent.vue

---
+++ src/components/NotFoundContent.vue
@@ -0,0 +1,25 @@
+<template>
+	<v-container>
+		<v-row>
+			<v-spacer />
+			<v-col :cols="3" align-self="middle" class="text-center">
+				<v-img src="@/assets/ManWithBox.svg" />
+				<v-spacer />
+				<h1 class="mt-12 mb-4 text-h4">Nessun risultato</h1>
+				<v-spacer />
+				<p class="!text-subtitle-2 grey--text">
+					Prova a sistemare la ricerca o i filtri in modo da trovare ciò che stai cercando.
+				</p>
+			</v-col>
+			<v-spacer />
+		</v-row>
+	</v-container>
+</template>
+
+<script>
+export default {
+	name: 'NotFoundContent'
+}
+</script>
+
+<style scoped></style>
