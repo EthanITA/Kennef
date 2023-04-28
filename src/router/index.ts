885519725f99e2b26f847a7256e4cb5238d9819e
madong <viaimperiale@gmail.com>
2023-04-28 17:38:04 +0200

    fix(route): missing View

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
@@ -8,66 +7,63 @@ const routes = [
 	{
 		path: '/',
 		name: 'home',
-		component: HomeView,
-		children: [
-			{
-				path: 'search',
-				name: 'search',
-				component: () => import(/* webpackChunkName: "search" */ '../views/SearchView.vue')
-			},
-			{
-				path: 'product/:id',
-				name: 'product',
-				component: () => import(/* webpackChunkName: "product" */ '../views/ProductView.vue')
-			},
-			{
-				path: 'categories',
-				name: 'categories',
-				component: () => import(/* webpackChunkName: "categories" */ '../views/CategoriesView.vue')
-			},
-			{
-				path: 'account',
-				name: 'account',
-				component: () => import(/* webpackChunkName: "account" */ '../views/AccountView.vue'),
-				children: [
-					{
-						path: 'profile',
-						name: 'profile',
-						component: () => import(/* webpackChunkName: "profile" */ '../views/ProfileView.vue')
-					},
-					{
-						path: 'orders',
-						name: 'orders',
-						component: () => import(/* webpackChunkName: "orders" */ '../views/OrdersView.vue'),
-						children: [
-							{
-								path: 'order/:id'
-							}
-						]
-					},
-					{
-						path: 'security',
-						name: 'security',
-						component: () => import(/* webpackChunkName: "security" */ '../views/SecurityView.vue')
-					}
-				]
-			},
-			{
-				path: 'cart',
-				name: 'cart',
-				component: () => import(/* webpackChunkName: "cart" */ '../views/CartView.vue')
-			},
-			{
-				path: 'checkout',
-				name: 'checkout',
-				component: () => import(/* webpackChunkName: "checkout" */ '../views/CheckoutView.vue')
-			},
-			{
-				path: 'order-completed',
-				name: 'order-completed',
-				component: () => import(/* webpackChunkName: "order-complete" */ '../views/OrderCompletedView.vue')
-			}
-		]
+		component: () => import(/* webpackChunkName: "home" */ '../views/HomeView.vue')
+	},
+	{
+		path: '/search',
+		name: 'search',
+		component: () => import(/* webpackChunkName: "search" */ '../views/SearchView.vue')
+	},
+	{
+		path: '/product/:id',
+		name: 'product',
+		component: () => import(/* webpackChunkName: "product" */ '../views/ProductView.vue')
+	},
+	{
+		path: '/categories',
+		name: 'categories',
+		component: () => import(/* webpackChunkName: "categories" */ '../views/CategoriesView.vue')
+	},
+	{
+		path: '/account',
+		name: 'account',
+		component: () => import(/* webpackChunkName: "account" */ '../views/AccountView.vue')
+	},
+	{
+		path: '/account/profile',
+		name: 'profile',
+		component: () => import(/* webpackChunkName: "profile" */ '../views/ProfileView.vue')
+	},
+	{
+		path: '/account/orders',
+		name: 'orders',
+		component: () => import(/* webpackChunkName: "orders" */ '../views/OrdersView.vue')
+	},
+	{
+		path: '/account/order/:id',
+		name: 'order',
+		component: () => import(/* webpackChunkName: "order" */ '../views/OrderView.vue')
+	},
+	{
+		path: '/account/security',
+		name: 'security',
+		component: () => import(/* webpackChunkName: "security" */ '../views/SecurityView.vue')
+	},
+
+	{
+		path: '/cart',
+		name: 'cart',
+		component: () => import(/* webpackChunkName: "cart" */ '../views/CartView.vue')
+	},
+	{
+		path: '/checkout',
+		name: 'checkout',
+		component: () => import(/* webpackChunkName: "checkout" */ '../views/CheckoutView.vue')
+	},
+	{
+		path: '/order-completed',
+		name: 'order-completed',
+		component: () => import(/* webpackChunkName: "order-complete" */ '../views/OrderCompletedView.vue')
 	}
 ]
 
---
+++ src/views/OrderView.vue
@@ -0,0 +1,9 @@
+<template></template>
+
+<script>
+export default {
+	name: 'OrderView'
+}
+</script>
+
+<style scoped></style>
