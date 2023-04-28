e67ef1d3c9b603c167cf3541f1963a095f2736f7
madong <viaimperiale@gmail.com>
2023-04-28 19:47:37 +0200

    feat: add Kennef Button

---
+++ src/components/kennef/Button.vue
@@ -0,0 +1,13 @@
+<template>
+	<v-btn :outlined="outlined" class="rounded-0 font-weight-black" color="primary" depressed x-large>
+		<slot />
+	</v-btn>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+	outlined?: boolean
+}>()
+</script>
+
+<style scoped></style>
