ed7ef30686f05dd97d02101a62cae620c46d1543
madong <viaimperiale@gmail.com>
2023-04-29 17:10:31 +0200

    feat(Category): add Component to View

--- src/views/CategoriesView.vue
+++ src/views/CategoriesView.vue
@@ -1,7 +1,13 @@
 <template>
-	<div>a</div>
+	<div>
+		<CategoriesLanding />
+		<VCategories />
+	</div>
 </template>
 
-<script setup></script>
+<script setup>
+import VCategories from '@/components/Categories/VCategories.vue'
+import CategoriesLanding from '@/components/Categories/CategoriesLanding.vue'
+</script>
 
 <style scoped></style>
