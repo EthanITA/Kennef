646694fe103f04b8017dfdc935a359cad1b8955e
madong <viaimperiale@gmail.com>
2023-04-29 16:56:00 +0200

    refactor(Landing): CardAction as a component

--- src/components/Home/Landing.vue
+++ src/components/Home/Landing.vue
@@ -2,13 +2,13 @@
 	<v-container class="pt-10 px-16">
 		<v-row>
 			<v-col class="d-flex pt-16">
-				<v-sheet class="justify-start px-16" color="white" elevation="0">
-					<h1 class="text-h3 font-weight-regular">Dal bullone in su</h1>
-					<p class="font-weight-bold mt-6">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. In etiam adipiscing ac congue.
-					</p>
-					<Button> Vai allo shop </Button>
-				</v-sheet>
+				<LandingCardAction>
+					<template #title> Dal bullone in su </template>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In etiam adipiscing ac congue.</p>
+					<template #action>
+						<Button> Vai allo shop </Button>
+					</template>
+				</LandingCardAction>
 			</v-col>
 			<v-col class="d-flex justify-end">
 				<v-img max-width="700" src="@/assets/BigClaw.png" />
@@ -29,4 +29,5 @@
 import Brands from '@/components/Home/Brands.vue'
 import KennefCharacteristics from '@/components/Home/KennefCharacteristics.vue'
 import Button from '@/components/kennef/Button.vue'
+import LandingCardAction from '@/components/LandingCardAction.vue'
 </script>
---
+++ src/components/LandingCardAction.vue
@@ -0,0 +1,11 @@
+<template>
+	<v-sheet class="justify-start px-16" color="white" elevation="0">
+		<h1 class="text-h3 font-weight-regular">
+			<slot name="title" />
+		</h1>
+		<p class="font-weight-bold mt-6">
+			<slot />
+		</p>
+		<slot name="action" />
+	</v-sheet>
+</template>
