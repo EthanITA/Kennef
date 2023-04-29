36f35eac592f3f4b31e6083491155ca1ec300c92
madong <viaimperiale@gmail.com>
2023-04-29 16:31:49 +0200

    fix(Landing): adjust margins

--- src/components/Home/Landing.vue
+++ src/components/Home/Landing.vue
@@ -1,17 +1,17 @@
 <template>
 	<v-container class="pt-10 px-16">
 		<v-row>
-			<v-col class="d-flex">
+			<v-col class="d-flex pt-16">
 				<v-sheet class="justify-start px-16" color="white" elevation="0">
 					<h1 class="text-h3 font-weight-regular">Dal bullone in su</h1>
-					<p class="font-weight-bold ml-1 mt-6">
+					<p class="font-weight-bold mt-6">
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit. In etiam adipiscing ac congue.
 					</p>
 					<Button> Vai allo shop </Button>
 				</v-sheet>
 			</v-col>
 			<v-col class="d-flex justify-end">
-				<v-img class="" max-width="700" src="@/assets/BigClaw.png"></v-img>
+				<v-img max-width="700" src="@/assets/BigClaw.png" />
 			</v-col>
 		</v-row>
 
