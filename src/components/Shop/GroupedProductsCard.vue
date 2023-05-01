60e921b15a163dc7bfac830d39c46ceab0e8326d
madong <viaimperiale@gmail.com>
2023-05-02 01:26:52 +0200

    refactor(GroupedProductCard): products give from VShop

--- src/components/Shop/GroupedProductsCard.vue
+++ src/components/Shop/GroupedProductsCard.vue
@@ -1,7 +1,7 @@
 <template>
 	<v-container>
 		<v-row no-gutters>
-			<v-col v-for="product in productList" :key="product.id" class="pa-0" cols="4">
+			<v-col v-for="product in products" :key="product.id" class="pa-0" cols="4">
 				<ProductCard :key="product.id" :product="product" />
 			</v-col>
 		</v-row>
@@ -12,6 +12,7 @@
 import ProductCard from '@/components/Shop/ProductCard.vue'
 import Product from '@/models/Product'
 
-const prod = new Product('Camicia', 'Abbigliamento', 10, 'description', 'https://picsum.photos/200/300', 'Camicia', 12)
-const productList = [prod, prod, prod, prod, prod, prod, prod]
+const props = defineProps<{
+	products: Product[]
+}>()
 </script>
--- src/components/Shop/VShop.vue
+++ src/components/Shop/VShop.vue
@@ -2,7 +2,7 @@
 	<v-row>
 		<v-col :cols="2" />
 		<v-col>
-			<GroupedProductsCard />
+			<GroupedProductsCard :products="productList" />
 		</v-col>
 		<v-col :cols="2" />
 	</v-row>
@@ -10,4 +10,8 @@
 
 <script lang="ts" setup>
 import GroupedProductsCard from '@/components/Shop/GroupedProductsCard.vue'
+import Product from '@/models/Product'
+
+const prod = new Product('Camicia', 'Abbigliamento', 10, 'description', 'https://picsum.photos/200/300', 'Camicia', 12)
+const productList = [prod, prod, prod, prod, prod, prod, prod]
 </script>
