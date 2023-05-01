1c868bc6f3569454851d1f20b47f0ddd57677f09
madong <viaimperiale@gmail.com>
2023-05-01 22:13:37 +0200

    feat: add Categories

---
+++ src/components/Categories/CategoryCard.vue
@@ -0,0 +1,19 @@
+<template>
+	<HoverScale>
+		<v-img :src="category.img" />
+
+		<div class="text-center text-subtitle-1 mt-8">
+			{{ category.name }}
+		</div>
+	</HoverScale>
+</template>
+<script lang="ts" setup>
+import HoverScale from '@/components/HoverScale.vue'
+
+const props = defineProps<{
+	category: {
+		name: string
+		img: string
+	}
+}>()
+</script>
--- src/components/Categories/VCategories.vue
+++ src/components/Categories/VCategories.vue
@@ -1,44 +1,35 @@
 <template>
 	<v-container fluid>
-		<h3 class="text-center text-h4 font-weight-regular">Categories</h3>
-		<v-row class="py-8" justify="center" no-gutters>
-			<v-col v-for="category in categories" :key="category.name" class="d-flex" cols="12" lg="2" md="2" xl="2">
-				<v-hover v-slot="{ hover }">
-					<!-- OPACITY TRANSITION ON HOVER IMG ON FRONT, BACK LIGHT GREY -->
-					<!-- <v-img :src="category.img" width="100"> -->
-					<v-expand-transition>
-						<div
-							v-if="hover"
-							class="d-flex transition-fast-in-fast-out primary v-card--reveal text-subtitle-1 white--text"
-							style="height: 100%"
-						>
-							{{ category.name }}
-						</div>
-					</v-expand-transition>
-					<!-- </v-img> -->
-				</v-hover>
+		<h3 class="text-center text-h4 font-weight-regular">Categorie</h3>
+		<v-row class="py-8" justify="center">
+			<v-col :cols="2" />
+			<v-col v-for="category in categories" :key="category.name" :cols="2">
+				<CategoryCard :category="category" />
 			</v-col>
+			<v-col :cols="2" />
 		</v-row>
 	</v-container>
 </template>
 
 <script lang="ts" setup>
+import CategoryCard from '@/components/Categories/CategoryCard.vue'
+
 const categories = [
 	{
-		name: 'Abbigliamento'
-		// img: require("../assets/categories/Abbigliamento.png")
+		name: 'Abbigliamento',
+		img: require('@/assets/categories/Abbigliamento.png')
 	},
 	{
-		name: 'Automotive'
-		// img: require("../assets/categories/Automotive.png")
+		name: 'Automotive',
+		img: require('@/assets/categories/Automotive.png')
 	},
 	{
-		name: 'Metallo'
-		// img: require("../assets/categories/Metallo.png")
+		name: 'Metallo',
+		img: require('@/assets/categories/Metallo.png')
 	},
 	{
-		name: 'Utensili'
-		// img: require("../assets/categories/Utensili.png")
+		name: 'Utensili',
+		img: require('@/assets/categories/Utensili.png')
 	}
 ]
 </script>
