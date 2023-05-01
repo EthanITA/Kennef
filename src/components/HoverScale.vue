4b01e86278879a5f4035836a0f93a90ef453041f
madong <viaimperiale@gmail.com>
2023-05-01 22:13:22 +0200

    feat: add HoverScale.vue

---
+++ src/components/HoverScale.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="hover-scale">
+		<slot />
+	</div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="sass" scoped>
+$transition: transform 0.3s ease
+$transform: scale(1.1)
+$backgroundColor: rgba(134, 134, 134, 0.1)
+.hover-scale
+  transition: $transition
+  &:hover
+    transform: $transform
+    background-color: $backgroundColor
+</style>
