ebc9c9e582574c839bb3af1f18e93f8e61faed47
madong <viaimperiale@gmail.com>
2023-05-10 22:12:09 +0200

    feat(Landing): add TopSellerProducts.vue

--- src/components/Home/Landing.vue
+++ src/components/Home/Landing.vue
@@ -15,7 +15,10 @@
 			</v-col>
 		</v-row>
 
-		<v-lazy>
+		<v-lazy v-if="topProducts.length">
+			<TopSellerProducts :products="topProducts.slice(0, 3)" class="mt-16" />
+		</v-lazy>
+		<v-lazy class="mt-16">
 			<KennefCharacteristics />
 		</v-lazy>
 
@@ -30,4 +33,41 @@ import Brands from '@/components/Home/Brands.vue'
 import KennefCharacteristics from '@/components/Home/KennefCharacteristics.vue'
 import Button from '@/components/kennef/Button.vue'
 import LandingCardAction from '@/components/LandingCardAction.vue'
+import { onMounted, ref } from 'vue'
+import Product from '@/models/Product'
+import TopSellerProducts from '@/components/Home/TopSellerProducts.vue'
+
+const topProducts = ref<Product[]>([])
+
+onMounted(() => {
+	topProducts.value = [
+		new Product(
+			'1',
+			'Cacciavite a croce \n' + 'FF Group',
+			10,
+			'AAA',
+			require('@/assets/examples/screwdriver.png'),
+			'Categoria',
+			5
+		),
+		new Product(
+			'2',
+			'Cacciavite a croce \n' + 'FF Group',
+			10,
+			'AAA',
+			require('@/assets/examples/screwdriver.png'),
+			'Categoria',
+			5
+		),
+		new Product(
+			'3',
+			'Cacciavite a croce \n' + 'FF Group',
+			10,
+			'AAA',
+			require('@/assets/examples/screwdriver.png'),
+			'Categoria',
+			5
+		)
+	]
+})
 </script>
---
+++ src/components/Home/TopSellerProducts.vue
@@ -0,0 +1,25 @@
+<template>
+	<v-container>
+		<v-row>
+			<v-col class="text-h4"> Top Seller</v-col>
+			<v-col class="text-right">
+				<Button medium outlined><span>S</span><span class="text-lowercase">copri di più</span></Button>
+			</v-col>
+		</v-row>
+		<v-row>
+			<GroupedProductsCard :products="products" />
+		</v-row>
+	</v-container>
+</template>
+
+<script lang="ts" setup>
+import Product from '@/models/Product'
+import Button from '@/components/kennef/Button.vue'
+import GroupedProductsCard from '@/components/Shop/GroupedProductsCard.vue'
+
+const props = defineProps<{
+	products: Product[]
+}>()
+</script>
+
+<style scoped></style>
