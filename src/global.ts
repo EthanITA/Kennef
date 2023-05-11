1c7fbe1fc99eef0b8958ced84a87ebddd2c572fe
madong <viaimperiale@gmail.com>
2023-05-11 22:55:28 +0200

    add: globals.ts that contains global consts

---
+++ src/global.ts
@@ -0,0 +1,14 @@
+export const brands = [
+	'Autel',
+	'Beta',
+	'Comet',
+	'Fasano Tools',
+	'Fervi',
+	'Fiac',
+	'Lavor',
+	'Milwaukee',
+	'Omcn',
+	'Total Utensili',
+	'U. Power',
+	'Utility Diadora'
+] as const
