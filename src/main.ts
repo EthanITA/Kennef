8bf78d67b4563d7e751462d7fa2c323d5a745778
madong <viaimperiale@gmail.com>
2023-08-09 22:24:41 +0200

    build: install pinia and tw

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,15 @@ import router from './router'
 import vuetify from './plugins/vuetify'
 import '@babel/polyfill'
 import './plugins/filters'
+import { createPinia, PiniaVuePlugin } from 'pinia'
+
+Vue.use(PiniaVuePlugin)
+const pinia = createPinia()
 
 Vue.config.productionTip = false
 new Vue({
 	router,
 	vuetify,
+	pinia,
 	render: (h) => h(App)
 }).$mount('#app')
