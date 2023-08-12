4ed31b9517d1734260f4c2050d6494626b9a8d9d
madong <viaimperiale@gmail.com>
2023-08-12 17:00:11 +0200

    feat(CheckoutSummary): integration with cart

--- src/components/Checkout/CheckoutSummary.vue
+++ src/components/Checkout/CheckoutSummary.vue
@@ -1,18 +1,18 @@
 <template>
 	<div class="flex-col gap-1">
-		<v-row v-for="product in products" align="center">
+		<v-row v-for="product in store.cart?.items" align="center">
 			<v-col class="flex-grow-0">
 				<div style="position: relative">
 					<v-img
 						:alt="product.name"
 						:aspect-ratio="1"
-						:src="product.image"
-						:style="{ 'display: none': !product.image }"
+						:src="store.medias?.[product.sku]?.[0]"
+						:style="{ 'display: none': !store.medias?.[product.sku]?.[0] }"
 						height="64"
 						style="background-color: #f6f6f6"
 						width="64"
 					/>
-					<v-badge :content="product.quantity" style="position: absolute; top: 8px; right: 8px" />
+					<v-badge :content="product.qty" style="position: absolute; top: 8px; right: 8px" />
 				</div>
 			</v-col>
 			<v-col>
@@ -33,11 +33,15 @@
 		<div class="flex-col gap-0.5">
 			<div class="d-flex justify-space-between align-center">
 				<p class="ma-0">Subtotale</p>
-				<Price :price="products.reduce((acc, cur) => acc + cur.price * cur.quantity, 0)" no-underline />
+				<Price :price="store.total?.subtotal_incl_tax || 0" no-underline />
 			</div>
 			<div class="d-flex justify-space-between align-center">
 				<p class="ma-0">Spedizione</p>
-				<Price v-if="shippingCost || shippingCost === 0" :price="shippingCost" no-underline />
+				<Price
+					v-if="store.total?.shipping_incl_tax"
+					:price="store.total?.shipping_incl_tax || 0"
+					no-underline
+				/>
 				<p v-else class="ma-0 text-caption grey--text text--darken-2">Calcolo nel prossimo step</p>
 			</div>
 		</div>
@@ -45,11 +49,7 @@
 		<div class="d-flex justify-space-between align-center">
 			<p class="ma-0">Totale</p>
 			<div class="d-flex align-baseline gap-0.5">
-				<Price
-					:price="products.reduce((acc, cur) => acc + cur.price * cur.quantity, 0) + (shippingCost || 0)"
-					class="text-h6"
-					no-underline
-				/>
+				<Price :price="store.total?.grand_total || 0" class="text-h6" no-underline />
 				<span v-if="completed" class="body-2 font-weight-light">IVA inclusa</span>
 			</div>
 		</div>
@@ -57,16 +57,18 @@
 </template>
 <script lang="ts" setup>
 //@ts-ignore
-import Product from '@/models/Product'
 import Price from '@/components/kennef/Price.vue'
 import Textfield from '@/components/kennef/Textfield.vue'
 import Button from '@/components/kennef/Button.vue'
+import { useCart } from '@/store/cart'
+import { onMounted } from 'vue'
 
-const products = Product.getRandomProducts()
+const store = useCart()
 const props = defineProps<{
 	shippingCost?: number
 	completed?: boolean
 }>()
+onMounted(store.getCart)
 </script>
 
 <style scoped></style>
