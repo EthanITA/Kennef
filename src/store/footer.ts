1af8aa6eb2d1700cc589a9ca36497d284fe2f55e
madong <viaimperiale@gmail.com>
2023-09-03 16:40:01 +0200

    fix(footer): default is shown

--- src/store/footer.ts
+++ src/store/footer.ts
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
 export const useFooter = defineStore('footer', () => {
-	const show = ref<boolean>(false)
+	const show = ref<boolean>(true)
 
 	const setShow = (value: boolean) => (show.value = value)
 
