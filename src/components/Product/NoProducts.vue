07e0e2fd91981f6b9e6e6de4af6b1f954822cebb
madong <viaimperiale@gmail.com>
2023-09-03 16:40:54 +0200

    fix(NoProducts): use tw class

--- src/components/Product/NoProducts.vue
+++ src/components/Product/NoProducts.vue
@@ -1,18 +1,12 @@
 <template>
 	<v-container>
-		<v-row>
-			<v-spacer />
-			<v-col :cols="3" align-self="middle" class="text-center">
-				<v-img src="@/assets/ManWithBox.svg" />
-				<v-spacer />
-				<h1 class="mt-12 mb-4 text-h4">Nessun risultato</h1>
-				<v-spacer />
-				<p class="!text-subtitle-2 grey--text">
-					Prova a sistemare la ricerca o i filtri in modo da trovare ciò che stai cercando.
-				</p>
-			</v-col>
-			<v-spacer />
-		</v-row>
+		<div class="tw-flex tw-text-center tw-flex-col tw-items-center">
+			<v-img class="tw-w-[50%]" src="@/assets/ManWithBox.svg" />
+			<h1 class="mt-12 mb-4 text-h4">Nessun risultato</h1>
+			<p class="!text-subtitle-2 grey--text">
+				Prova a sistemare la ricerca o i filtri in modo da trovare ciò che stai cercando.
+			</p>
+		</div>
 	</v-container>
 </template>
 
