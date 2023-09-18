e0b2000da618d3a97dc20d9f545c6df023743009
madong <viaimperiale@gmail.com>
2023-09-18 20:55:37 +0200

    fix(CheckoutButton): checkout disabled if no items

--- src/components/Checkout/CheckoutButtons.vue
+++ src/components/Checkout/CheckoutButtons.vue
@@ -1,7 +1,7 @@
 <template>
 	<v-container>
 		<v-sheet class="pa-4 flex-col gap-0.5">
-			<Button @click="$router.push('/checkout')">checkout</Button>
+			<Button :disabled="!(store.cart?.items || []).length" @click="$router.push('/checkout')">checkout</Button>
 			<template v-if="false">
 				<PaypalButton />
 				<PaypalButton instalment />
@@ -13,6 +13,9 @@
 <script lang="ts" setup>
 import Button from '@/components/kennef/Button.vue'
 import PaypalButton from '@/components/Checkout/PaypalButton.vue'
+import { useCart } from '@/store/cart'
+
+const store = useCart()
 </script>
 
 <style scoped>
