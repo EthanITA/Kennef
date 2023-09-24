bb06d76b63d784680d0ffa672b33d5b4d21692e0
madong <viaimperiale@gmail.com>
2023-09-24 23:31:36 +0200

    fix(Brands): use grid instead v-row

--- src/components/Home/Brands.vue
+++ src/components/Home/Brands.vue
@@ -1,21 +1,16 @@
 <template>
 	<div class="d-flex flex-column brand-list pt-16">
 		<h2 class="mb-11 text-h5 font-weight-regular align-self-center">Cerca tra i migliori brand</h2>
-		<v-row class="flex-child mb-16">
-			<v-col
+		<div class="tw-grid-cols-6 tw-grid flex-child mb-16">
+			<router-link
 				v-for="brand in store.brands"
 				:key="brand.page_title"
 				v-ripple
+				:to="`/shop?brand=${brand.option_id}`"
 				class="d-flex selected tw-cursor-pointer"
-				cols="4"
-				lg="2"
-				md="2"
-				xl="2"
-				@click="$router.push(`/shop?brand=${brand.option_id}`)"
-			>
-				<v-img :src="brand.image" class="mr-10"></v-img>
-			</v-col>
-		</v-row>
+				><v-img :src="brand.image" class="mr-10"></v-img>
+			</router-link>
+		</div>
 	</div>
 </template>
 
